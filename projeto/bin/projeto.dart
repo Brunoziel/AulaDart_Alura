6c7dc5ebfe180fdd7cbf8f2bae5343bb1156814e
unknown <onix.brr@gmail.com>
2023-12-15 00:23:37 -0300

    Repetições e comentários

--- projeto/bin/projeto.dart
+++ projeto/bin/projeto.dart
@@ -2,11 +2,21 @@ import 'package:projeto/projeto.dart' as projeto;
 
 void main() {
 
-  int numero = 2;
+  int idade = 21;
   const String nome = "Bruno";
 
   //O termo "Final" funciona como const, mas pode ser indefinido inicialmente
   final String fruta;
+  bool maiorDeIdade;
+  int energia = 100;
+
+  if(idade >= 18 ) {
+    maiorDeIdade = true;
+  } else{
+    maiorDeIdade = false;
+    }
+
+  
 
   print('Hello $nome');
 
@@ -20,6 +30,17 @@ void main() {
 
   fruta = "maça";
 
-  print("Meu nome é ${Listanomes[0]} e tenho $numero anos e gosto de $fruta");
+  print("Meu nome é ${Listanomes[0]} e tenho $idade anos e gosto de $fruta \nSou maior de idade ? $maiorDeIdade");
 
+  for(int i=0; i<5; i++){
+    print("Conclui $i voltas");
+  }
+
+  //Metodo para retirar energia
+  while(energia>0){
+    print("Mais uma repetição");
+    energia = energia - 10;
+  }
+
+  //TODO: Fazer outros testes
 }
