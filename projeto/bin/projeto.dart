f2d18fc946b6fab7c09536c793f9ddb010b00704
unknown <onix.brr@gmail.com>
2023-12-14 22:43:10 -0300

    Listas

--- projeto/bin/projeto.dart
+++ projeto/bin/projeto.dart
@@ -1,5 +1,25 @@
 import 'package:projeto/projeto.dart' as projeto;
 
-void main(List<String> arguments) {
-  print('Hello world: ${projeto.calculate()}!');
+void main() {
+
+  int numero = 2;
+  const String nome = "Bruno";
+
+  //O termo "Final" funciona como const, mas pode ser indefinido inicialmente
+  final String fruta;
+
+  print('Hello $nome');
+
+  //Uma boa prática é sempre colocar o tipo dos objetos, pois facilitará o acesso dele ao dart
+
+  List <String> Listanomes = ["Bruno", "Italo", "Maria", "Bia"];
+
+  print(Listanomes);
+  print(Listanomes[0]);
+  print(Listanomes.length);
+
+  fruta = "maça";
+
+  print("Meu nome é ${Listanomes[0]} e tenho $numero anos e gosto de $fruta");
+
 }
